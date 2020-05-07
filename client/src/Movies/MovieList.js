2bb36cf639d632565b031cab130676f8fb5b049e
ClydeFrog04 <egan.randal@gmail.com>
2020-05-06 18:24:11 -0600

    mvp met

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,35 +1,38 @@
 import React from 'react';
+import {Link, NavLink} from "react-router-dom";
 
 const MovieList = props => {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="movie-list">
+            {props.movies.map(movie => (
+                <Link key={movie.id} to={`/movies/${movie.id}`} style={{ textDecoration: 'none', color:"black"}}>{/*//prevent the gross purple, and remove underline*/}
+                    <MovieDetails key={movie.id} movie={movie}/>
+                </Link>
+            ))}
+        </div>
+    );
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+function MovieDetails({movie}) {
+    const {title, director, metascore, stars} = movie;
+    return (
+        <div className="movie-card">
+            <h2>{title}</h2>
+            <div className="movie-director">
+                Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+                Metascore: <strong>{metascore}</strong>
+            </div>
+            <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+            {stars.map(star => (
+                <div key={star} className="movie-star">
+                    {star}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default MovieList;
